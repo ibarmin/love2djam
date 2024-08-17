4759b33ce0ccf6963312a738c1fee75776d7883d
Nikolai Baklanov <n.baklanov@vk.team>
2024-08-17 12:58:45 +0300

    Startup

---
+++ Assets/Scripts/Consts/SceneNumbers.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneNumbers : MonoBehaviour
+{
+    public static int MAIN_SCENE_ID = 0;
+}
---
+++ Assets/Scripts/MainScene/MainSceneUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSceneUI : MonoBehaviour
+{
+    private SceneLoader sceneLoader = new SceneLoader();
+
+
+    public void onExitGame() {
+        Application.Quit();
+    }
+
+    public void onStartGame() {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
